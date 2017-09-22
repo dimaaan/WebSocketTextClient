0c888fdf3e5087249ecfa3cbb90b0a2057e89830
dimaaan <dimaaan@gmail.com>
2017-09-23 00:46:25 +0300

    add WebSocketTextClient Solution

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # WebSocketTextClient
-Wrapper around System.Net.WebSockets.ClientWebSocket that provides event based interface to exchange text messages over Web Sockets. Support cancellation
+
+Wrapper around System.Net.WebSockets.ClientWebSocket that provides event based interface to exchange text messages over Web Sockets and support cancellation.
+
+Example:
+
+```csharp
+using (var cts = new CancellationTokenSource())
+using (var client = new WebSocketTextClient(cts.Token))
+{
+    client.OnResponse = (text) => Console.WriteLine(text);
+
+    await client.Connect(new Uri("ws://example.com"));
+    await client.Send("ping");
+    await Task.Delay(5000);
+}
+```
---
+++ WebSocketTextClient/WebSocketTextClient.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebSockets
+{
+    /// <summary>
+    /// Wrapper around ClientWebSocket that provides suitable interface to exchange text messages over Web Sockets
+    /// </summary>
+    /// <seealso cref="System.Net.WebSockets.ClientWebSocket"/>
+    public class WebSocketTextClient: IDisposable
+    {
+        private ClientWebSocket Socket;
+        private CancellationToken Cancellation;
+        private Task RecieveTask;
+        private bool Disposed = false; // To detect redundant calls
+
+        public WebSocketTextClient(CancellationToken cancellation)
+        {
+            Cancellation = cancellation;
+            Socket = new ClientWebSocket();
+            RecieveTask = new Task(RecieveLoop, cancellation);
+        }
+
+        public event Action<string> OnResponse;
+
+        public async Task Connect(Uri url)
+        {
+            await Socket.ConnectAsync(url, Cancellation);
+            RecieveTask.Start();
+        }
+
+        public Task Send(string str)
+        {
+            var bytes = Encoding.UTF8.GetBytes(str);
+            return Socket.SendAsync(new ArraySegment<byte>(bytes), WebSocketMessageType.Text, true, Cancellation);
+        }
+
+        private async void RecieveLoop()
+        {
+            byte[] buffer = new byte[1024];
+            ArraySegment<byte> writeSegment;
+            WebSocketReceiveResult result;
+
+            try
+            {
+                while (true)
+                {
+                    writeSegment = new ArraySegment<byte>(buffer);
+                    do
+                    {
+                        result = await Socket.ReceiveAsync(writeSegment, Cancellation);
+                        writeSegment = new ArraySegment<byte>(buffer, writeSegment.Offset + result.Count, writeSegment.Count - result.Count);
+                    } while (!result.EndOfMessage);
+
+                    var responce = Encoding.UTF8.GetString(buffer, 0, writeSegment.Offset);
+                    OnResponse?.Invoke(responce);
+                }
+            }
+            catch (AggregateException e) when (e.InnerException is TaskCanceledException)
+            {
+                // swallow cancel exception
+            }
+        }
+
+        public void Dispose()
+        {
+            Socket.Dispose();
+            RecieveTask.Dispose();
+        }
+    }
+}
