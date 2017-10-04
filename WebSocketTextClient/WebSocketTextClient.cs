3b9f53221de59fd5a17c85617c1ceeb0255576df
dimaaan <dimaaan@gmail.com>
2017-10-04 20:55:48 +0300

    1. Add ability to specify receive buffer size. 2. Add auto doubling buffer size on overflow. 3. Make cancellation token optional. 4. Change version to 2.2.0.

--- WebSocketTextClient/WebSocketTextClient.cs
+++ WebSocketTextClient/WebSocketTextClient.cs
@@ -15,16 +15,29 @@ namespace WebSockets
         private ClientWebSocket Socket;
         private CancellationToken CancellationToken;
         private Task RecieveTask;
+        private readonly int InitialRecieveBufferSize;
+        private readonly bool AutoIncreaseRecieveBuffer;
 
         /// <summary>
         /// Create new instance of WebSocketTextClient
         /// </summary>
         /// <param name="cancellationToken">Cancels pending send and receive operations</param>
-        public WebSocketTextClient(CancellationToken cancellationToken)
+        /// <param name="initialRecieveBufferSize">Socket initial receive buffer size in bytes</param>
+        /// <param name="autoIncreaseRecieveBuffer">
+        /// If true, receive buffer size will be doubled on overflow. 
+        /// If false, unobserved InvalidOperationException will be thrown on overflow
+        /// </param>
+        public WebSocketTextClient(CancellationToken? cancellationToken = null, int initialRecieveBufferSize = 1024, bool autoIncreaseRecieveBuffer = true)
         {
-            CancellationToken = cancellationToken;
+            if (initialRecieveBufferSize <= 0)
+                throw new ArgumentException("Receive buffer size should be greater than zero", nameof(initialRecieveBufferSize));
+
+            CancellationToken = cancellationToken ?? CancellationToken.None;
+            InitialRecieveBufferSize = initialRecieveBufferSize;
+            AutoIncreaseRecieveBuffer = autoIncreaseRecieveBuffer;
+
             Socket = new ClientWebSocket();
-            RecieveTask = new Task(RecieveLoop, cancellationToken);
+            RecieveTask = new Task(RecieveLoop, CancellationToken);
         }
 
         /// <summary>
@@ -54,7 +67,7 @@ namespace WebSockets
 
         private async void RecieveLoop()
         {
-            byte[] buffer = new byte[1024];
+            byte[] buffer = new byte[InitialRecieveBufferSize];
             ArraySegment<byte> writeSegment;
             WebSocketReceiveResult result;
 
@@ -67,6 +80,21 @@ namespace WebSockets
                     {
                         result = await Socket.ReceiveAsync(writeSegment, CancellationToken);
                         writeSegment = new ArraySegment<byte>(buffer, writeSegment.Offset + result.Count, writeSegment.Count - result.Count);
+
+                        // check buffer overflow
+                        if(!result.EndOfMessage && writeSegment.Count == 0)
+                        {
+                            if (AutoIncreaseRecieveBuffer)
+                            {
+                                Array.Resize(ref buffer, buffer.Length * 2);
+                                writeSegment = new ArraySegment<byte>(buffer, writeSegment.Offset, buffer.Length - writeSegment.Offset);
+                            }
+                            else
+                            {
+                                throw new InvalidOperationException($"Socket receive buffer overflow. Buffer size = {buffer.Length}. Buffer auto-increase = {AutoIncreaseRecieveBuffer}");
+                            }
+                        }
+                            
                     } while (!result.EndOfMessage);
 
                     var responce = Encoding.UTF8.GetString(buffer, 0, writeSegment.Offset);
